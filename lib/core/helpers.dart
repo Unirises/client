0f332e8ad3ec3baefeac646ec538b324dc28ac1b
Kirk Charles Niverba <kirkniverba23@gmail.com>
2021-05-04 12:08:19 +0800

    fix compute fare

--- lib/core/helpers.dart
+++ lib/core/helpers.dart
@@ -1,30 +1,31 @@
-  num computeFare(String? type, num? distance, bool isAddtlStop) {
-    var baseFare = 0;
-    var addtlStop = 0;
-    late var perKm;
-    switch (type) {
-      case 'motorcycle':
-        baseFare = 50;
-        addtlStop = 40;
-        perKm = 7;
-        break;
-      case 'car2Seater':
-        baseFare = 175;
-        addtlStop = 45;
-        perKm = 20;
-        break;
-      case 'car4Seater':
-        baseFare = 350;
-        addtlStop = 50;
-        perKm = 25;
-        break;
-      case 'car7Seater':
-        baseFare = 460;
-        addtlStop = 50;
-        perKm = 30;
-        break;
-    }
-    return baseFare +
-        (perKm * (distance! / 1000)) +
-        (isAddtlStop ? addtlStop : 0);
+num computeFare(String? type, num? distance, bool isAddtlStop) {
+  var baseFare = 0;
+  var addtlStop = 0;
+  late var perKm;
+  switch (type) {
+    case 'motorcycle':
+      baseFare = 50;
+      addtlStop = 40;
+      perKm = 7;
+      break;
+    case 'car2Seater':
+      baseFare = 175;
+      addtlStop = 45;
+      perKm = 20;
+      break;
+    case 'car4Seater':
+      baseFare = 350;
+      addtlStop = 50;
+      perKm = 25;
+      break;
+    case 'car7Seater':
+      baseFare = 460;
+      addtlStop = 50;
+      perKm = 30;
+      break;
   }
+
+  distance = (distance! / 1000) - 5;
+  return (baseFare + (perKm * distance) + (isAddtlStop ? addtlStop : 0))
+      .round();
+}
